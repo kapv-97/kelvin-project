e882b2115456f97e570150782b487bae21ec6a12
kapv-97 <114553864+kapv-97@users.noreply.github.com>
2022-09-28 00:24:46 -0400

    Create main.js

---
+++ main.js
@@ -0,0 +1,13 @@
+// The forecast today is 293 Kelvin
+const kelvin = 293;
+
+// Celsius is 273 degrees less than Kelvin
+var celsius = kelvin - 273;
+
+// Equation to convert Celsius into fahrenheit
+var fahrenheit = celsius * (9/5) + 32;
+
+// 0 Kelvin is 67 degrees fahrenheit
+var TEMPERATURE = 67;
+
+console.log(`The temperature is ${TEMPERATURE} degrees fahrenheit`);
